1f83e6d39483b32457dae33eba6df433146d6597
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-25 01:13:17 +0700

    Add example split string using StringTokenizer

--- JavaUtilIOPackage/src/StringTokenizer.java
+++ JavaUtilIOPackage/src/StringTokenizer.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 
 public class StringTokenizer {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
+		System.out.println("Input a number phone: ");
+		String numberPhone = new Scanner(System.in).nextLine();
+		//String numberPhone = "(091) 022-6758080";
+		java.util.StringTokenizer st = new java.util.StringTokenizer(numberPhone, "()-");
+		System.out.println("Nation Code: " + st.nextToken().trim());
+		System.out.println("Region Code: " + st.nextToken().trim());
+		System.out.println("Number Phone: " + st.nextToken().trim());
 	}
-
 }
