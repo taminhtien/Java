44703a9232e10ab57f68d243293d6bd4f4b4af79
Tien Ta Minh <taminhtien1993@gmail.com>
2015-04-25 11:19:58 +0700

    Add Select Update syntax JDBC

--- JDBCExample/src/JDBC.java
+++ JDBCExample/src/JDBC.java
@@ -1,5 +1,7 @@
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
@@ -25,27 +27,66 @@ public class JDBC {
 			System.out.println("Connecting to database...");
 			conn = (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
 			System.out.println("Connected database...");
-			
+
 			// Execute a query
-			//System.out.println("Creating database...");
+			// System.out.println("Creating database...");
 			stmt = (Statement) conn.createStatement();
 
 			// String sql = "CREATE DATABASE TRAINEES";
 			// stmt.executeUpdate(sql);
 
-			String sql = "INSERT INTO traineeInfo "
-					+ "VALUES ('tientm', 'tientm', 'tientm@gmail.com')";
-			stmt.executeUpdate(sql);
-			
-			sql = "INSERT INTO traineeInfo "
-					+ "VALUES ('loint', 'loint', 'loint@gmail.com')";
-			stmt.executeUpdate(sql);
-			
-			sql = "INSERT INTO traineeInfo "
-					+ "VALUES ('thongvc', 'thongvc', 'thongvc@gmail.com')";
-			stmt.executeUpdate(sql);
-			
-			System.out.println("Records created successfully...");
+			/*
+			 * String sql = "INSERT INTO traineeInfo " +
+			 * "VALUES ('tientm', 'tientm', 'tientm@gmail.com')";
+			 * stmt.executeUpdate(sql);
+			 * 
+			 * sql = "INSERT INTO traineeInfo " +
+			 * "VALUES ('loint', 'loint', 'loint@gmail.com')";
+			 * stmt.executeUpdate(sql);
+			 * 
+			 * sql = "INSERT INTO traineeInfo " +
+			 * "VALUES ('thongvc', 'thongvc', 'thongvc@gmail.com')";
+			 * stmt.executeUpdate(sql);
+			 * 
+			 * System.out.println("Records created successfully...");
+			 */
+			System.out.println("Input your account: ");
+			String userName = "", pass = "", email = "";
+			while (userName == "" || pass == "" || email == "") {
+				System.out.print("User Name: ");
+				userName = new Scanner(System.in).nextLine();
+				System.out.print("Password: ");
+				pass = new Scanner(System.in).nextLine();
+				System.out.print("Email: ");
+				email = new Scanner(System.in).nextLine();
+			}
+			String sql = "SELECT * FROM traineeInfo WHERE username = \'"
+					+ userName + "\'";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			// Extract data from result set
+			String rsUserName = "", rsPass = "", rsEmail = "";
+			while (rs.next()) {
+				rsUserName = rs.getString("username");
+				rsPass = rs.getString("password");
+				rsEmail = rs.getString("email");
+				System.out.println("User Name: " + rsUserName);
+				System.out.println("Password: " + rsPass);
+				System.out.println("Email: " + rsEmail);
+			}
+
+			// Update new data
+			if (rs != null) {
+				sql = "UPDATE traineeInfo SET password = \'" + pass + "\', "
+						+ "email = \'" + email + "\' WHERE username = \'"
+						+ userName + "\'";
+				int rs2 = stmt.executeUpdate(sql);
+				if (rs2 == 1) {
+					System.out.println("Updated database..");
+				}
+			}
+
+			rs.close();
 		} catch (SQLException se) {
 			// Handle errors for JDBC
 			se.printStackTrace();
