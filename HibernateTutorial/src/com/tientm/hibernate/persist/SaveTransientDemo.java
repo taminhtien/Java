296c38ba2381df173d3708a31e48cd8f09233780
Tien Ta Minh <taminhtien1993@gmail.com>
2015-05-05 18:28:02 +0700

    Using save(Object) to transfer transient data to persistent data

--- HibernateTutorial/src/com/tientm/hibernate/persist/SaveTransientDemo.java
+++ HibernateTutorial/src/com/tientm/hibernate/persist/SaveTransientDemo.java
@@ -11,45 +11,57 @@ import com.tientm.hibernate.entities.Employee;
 import com.tientm.hibernate.entities.Timekeeper;
 import com.tientm.hibernate.query.HibernateUtils;
 
-public class PersistToTransientDemo {
+/**
+ * Change Transient data to Persistent data using save(Object)
+ * 
+ * @author tientm
+ *
+ */
+public class SaveTransientDemo {
 	private static DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
-	private static Timekeeper persist2Transient(Session session, Employee emp) {
+	private static Timekeeper persistTransient(Session session, Employee emp) {
 		// Hãy chú ý:
 		// timekeeperId cấu hình tự động được tạo ra bởi UUID.
 		// @GeneratedValue(generator = "uuid")
 		// @GenericGenerator(name = "uuid", strategy = "uuid2")
 		// Tạo một đối tượng, nó đang có tình trạng Transient.
-		Timekeeper tk1 = new Timekeeper();
-		tk1.setEmployee(emp);
-		tk1.setInOut(Timekeeper.IN);
-		tk1.setDateTime(new Date());
+		Timekeeper tk2 = new Timekeeper();
+		tk2.setEmployee(emp);
+		tk2.setInOut(Timekeeper.OUT);
+		tk2.setDateTime(new Date());
 
-		// Lúc này "tk1" đang có tình trạng Transient
-		System.out.println("- tk1 persistent? " + session.contains(tk1));
-		
-		System.out.println("- Call persist(tk1)...");
-		// Hibernate gắn Id cho "tk1", chưa có insert
-		session.persist(tk1);
-		
-		// tk1 đã được gắn ID
-		System.out.println("- tk1.getTimekeeperId() = " + tk1.getTimekeeperId());
+		// Lúc này "tk2" đang có tình trạng Transient
+		System.out.println("- tk2 persistent? " + session.contains(tk2));
 
-		// Lúc này "tk1" đã có trạng thái Persistent
+		System.out.println("- Call save(tk2)...");
+		// Sử dụng save function để chuyển đối tượng thành persistent
+		// save() rất giống với persist()
+		// save() trả về ID còn persist() là void.
+		// Hibernate gán Id vào 'tk2', sẽ chưa có insert gì cả
+		// Nó trả về ID của 'tk2'.
+		String id = (String) session.save(tk2);
+
+		// tk2 đã được gắn ID
+		System.out.println("- session.save(tk2) = " + id);
+		System.out
+				.println("- tk2.getTimekeeperId() = " + tk2.getTimekeeperId());
+
+		// Lúc này "tk2" đã có trạng thái Persistent
 		// Nó đã được quản lý trong Session
 		// Nhưng chưa có hành động gì insert xuống DB
-		System.out.println("- tk1 persistent? " + session.contains(tk1));
-		
+		System.out.println("- tk2 persistent? " + session.contains(tk2));
+
 		System.out.println("- Call flush...");
 		// Chủ động đẩy dữ liệu xuống DB, gọi flush().
 		// Nếu không gọi flush() dữ liệu sẽ được đẩy xuống tại lệnh commit().
 		// Lúc này mới có insert.
 		session.flush();
-		
-		System.out.println("- timekeeperId = " + tk1.getTimekeeperId());
-		System.out.println("- inOut = " + tk1.getInOut());
-		System.out.println("- dateTime = " + df.format(tk1.getDateTime()));
-		return tk1;
+
+		System.out.println("- timekeeperId = " + tk2.getTimekeeperId());
+		System.out.println("- inOut = " + tk2.getInOut());
+		System.out.println("- dateTime = " + df.format(tk2.getDateTime()));
+		return tk2;
 
 	}
 
@@ -61,7 +73,7 @@ public class PersistToTransientDemo {
 			session.getTransaction().begin();
 			emp = DataUtils.findEmployee(session, "E7499");
 			// Thêm vào bảng TimeKeeper thời gian hoạt động của emp
-			persist2Transient(session, emp);
+			persistTransient(session, emp);
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
