eb41fe36691ed9f1f19388e3370dc8859ee70dd5
Tien Ta Minh <taminhtien1993@gmail.com>
2015-05-06 15:21:54 +0700

    Demo configuration Servlet uses Annotation

---
+++ Servlet Tutorial/src/com/tientm/tutorial/servlet/HelloServlet.java
@@ -0,0 +1,42 @@
+package com.tientm.tutorial.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(urlPatterns = { "/Hello" }, initParams = { @WebInitParam(name = "name", value = "Guest") })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public HelloServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String name = this.getInitParameter("name");
+		ServletOutputStream out = response.getOutputStream();
+		out.print("<html>");
+		out.print("<head><title>Hello Servlet</title></head>");
+
+		out.print("<body>");
+		out.print("<h1>Hello " + name + "</h1>");
+		out.print("</body>");
+		out.print("<html>");
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
